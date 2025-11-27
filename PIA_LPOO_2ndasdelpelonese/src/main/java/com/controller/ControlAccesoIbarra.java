6f21172cdf0928a59010f031a749bf2d0f711a7f
semen power <semnupower.05@hotmail.com>
2025-11-26 20:08:56 -0600

    creacion de proyecto

---
+++ PIA_LPOO_2ndasdelpelonese/src/main/java/com/controller/ControlAccesoIbarra.java
@@ -0,0 +1,45 @@
+package com.controller;
+
+import com.model.Cliente;
+import com.model.Membresia;
+import com.util.GymException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ControlAccesoIbarra {
+    private List<String> registroAccesos;
+
+    public ControlAccesoIbarra() {
+        this.registroAccesos = new ArrayList<>();
+    }
+
+    public boolean validarEntrada(Cliente cliente) throws GymException {
+        Membresia mem = cliente.getMembresiaActual();
+        if (mem == null) {
+            throw new GymException("El cliente no tiene membresía activa.");
+        }
+
+        if (!mem.esValida()) {
+            throw new GymException("Membresía vencida. Por favor renovar.");
+        }
+
+        registrarEvento(cliente, "ENTRADA");
+        return true;
+    }
+
+    public void registrarSalida(Cliente cliente) {
+        registrarEvento(cliente, "SALIDA");
+    }
+
+    private void registrarEvento(Cliente c, String tipo) {
+        String log = String.format("[%s] %s - Cliente: %s (%s)",
+                LocalDateTime.now(), tipo, c.getNombreCompleto(), c.getId());
+        registroAccesos.add(log);
+        System.out.println(log);
+    }
+
+    public List<String> getHistorialAccesos() {
+        return registroAccesos;
+    }
+}
